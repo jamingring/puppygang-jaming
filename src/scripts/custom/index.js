02da7fdef4d1a544370b01eb74780850546c2ff0
jamingring <jamingring@outlook.com>
2019-08-25 12:39:29 -0400

    Updated: with latest

---
+++ src/scripts/custom/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $('.view_map--trigger').fancybox();
+});
---
+++ src/scripts/site.js
@@ -0,0 +1,4 @@
+// =require plugins/jquery.js
+// =require plugins/fancybox.js
+// =require origin/index.js
+// =require custom/index.js
